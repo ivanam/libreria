793a29a334b2858e29b8496d30aed746330dcfce
ivanam <iva.moyano@gmail.com>
2016-06-07 18:42:19 -0300

    primeros cambios libreria

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongodb = require('mongodb');
+var server = new mongodb.Server("localhost", 27017, {});
+var db = new mongodb.Db('libreria', server, {});
+
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var libro = require('./routes/libro');
 
 var app = express();
 
@@ -15,15 +19,22 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Headers", "Origin");
+    req.db = db;
+    next();
+})
+
 app.use('/', routes);
-app.use('/users', users);
+app.use('/libro', libro);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -56,5 +67,8 @@ app.use(function(err, req, res, next) {
   });
 });
 
+app.listen(3000,function(){
+  console.log("Started on PORT 3000");
+})
 
 module.exports = app;
