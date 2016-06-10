afd111ce9203f2c4d2cd34613dc4249ebc885070
Damián Mansilla <Damin Mansilla>
2016-06-10 19:29:41 -0300

    Inicios...

--- app.js
+++ app.js
@@ -1,40 +1,97 @@
-var express = require('express');
+
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var mongodb = require('mongodb');
-var server = new mongodb.Server("localhost", 27017, {});
-var db = new mongodb.Db('libreria', server, {});
-
 var routes = require('./routes/index');
-var libro = require('./routes/libro');
+var users = require('./routes/users');
 
-var app = express();
+var express     = require('express');  
+var app         = express();  
+var mongoose     = require('mongoose');
+
+// Conexión con la base de datos
+mongoose.connect('mongodb://localhost:27017/liberbook');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(function(req,res,next){
-    res.header("Access-Control-Allow-Origin","*");
-    res.header("Access-Control-Allow-Headers", "Origin");
-    req.db = db;
-    next();
-})
-
 app.use('/', routes);
-app.use('/libro', libro);
+app.use('/users', users);
+
+// Definición de modelos
+var Libro = mongoose.model('Libro', {  
+    text: String
+});
+// Rutas de nuestro API
+// GET de libro los Libro
+app.get('/api/libros', function(req, res) {  
+    console.log("app.get libros");
+    Libro.find(function(err, libros) {
+        if(err) {
+            res.send(err);
+        }
+        res.json(libros);
+    });
+});
+
+//debería meterlo en un modulo API
+// POST que crea un Libro y devuelve libros tras la creación
+app.post('/api/libros', function(req, res) {  
+    console.log("app.post libros");
+    Libro.create({
+        text: req.body.text,
+        done: false
+    }, function(err, libro){
+        if(err) {
+            res.send(err);
+        }
+
+        Libro.find(function(err, libros) {
+            if(err){
+                res.send(err);
+            }
+            res.json(libros);
+        });
+    });
+});
+
+// DELETE un Libro específico y devuelve libros tras borrarlo.
+app.delete('/api/libros/:libro', function(req, res) {  
+    console.log("app.delete libros");
+    Libro.remove({
+        _id: req.params.libro
+    }, function(err, libro) {
+        if(err){
+            res.send(err);
+        }
+
+        Libro.find(function(err, libros) {
+            if(err){
+                res.send(err);
+            }
+            res.json(libros);
+        });
+
+    })
+});
+
+// Carga una vista HTML simple donde irá nuestra Single App Page
+// Angular Manejará el Frontend
+app.get('*', function(req, res) {  
+    res.sendfile('./public/index.html');                
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -66,9 +123,7 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
-app.listen(3000,function(){
-  console.log("Started on PORT 3000");
-})
-
-module.exports = app;
+// Escucha en el puerto 8080 y corre el server
+app.listen(8080, function() {  
+    console.log('Liberbook corriendo en 8080');
+});
---
+++ public/javascripts/main.js
@@ -0,0 +1,40 @@
+angular.module('angularTodo', []);  
+function mainController($scope, $http) {  
+    $scope.formData = {};
+
+    // Cuando se cargue la página, pide del API Libro
+    $http.get('/api/libro')
+        .success(function(data) {
+            $scope.libro
+       = data;
+            console.log(data)
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+
+    // Cuando se añade un nuevo Libro, manda el texto a la API
+    $scope.createLibro = function(){
+        $http.post('/api/libros', $scope.formData)
+            .success(function(data) {
+                $scope.formData = {};
+                $scope.libros = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error:' + data);
+            });
+    };
+
+    // Borra un Libro despues de checkearlo como acabado
+    $scope.deleteLibro = function(id) {
+        $http.delete('/api/libros/' + id)
+            .success(function(data) {
+                $scope.libros = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error:' + data);
+            });
+    };
+}
