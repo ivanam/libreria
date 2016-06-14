a77f2c68f9dba86f39dea058e8d2c269e119c8a1
Damián Mansilla <Damin Mansilla>
2016-06-14 18:55:26 -0300

    Agregada plantillas con nav, dentro de view ver index.jade. TODO hipervinculos y seleccionar una paleta de colores apropiada

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', routes);
+app.use('/liberbook', routes);
 app.use('/users', users);
 
 // Definición de modelos
