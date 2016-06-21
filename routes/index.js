76144c12b122980a58dc2c8e544187141f2d7d03
Damián Mansilla <Damin Mansilla>
2016-06-21 20:36:59 -0300

    Incluído css y cuadrilla, falta modelos

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,26 @@ router.get('/', function(req, res, next) {
   res.render('index.jade');
 });
 
+router.get('/:title',function(req,res, next){
+
+	LibroModel.find({title:req.params.title}, function(err,docs){
+
+		
+		if (docs.length != 0){
+
+			var id = docs[0].title;
+
+			client.get("https://www.googleapis.com/books/v1/volumes?q="+id, function(data,response){
+		
+				//res.send(data["items"][0]);
+				res.render("listadoLibros.jade", {valores:data["items"]});
+			});
+
+		}else{
+			res.send("ESTE LIBRO NO SE ENCUENTRA DISPONIBLE EN LA BASE DE DATOS");
+		}
+
+	});
+});
+
 module.exports = router;
