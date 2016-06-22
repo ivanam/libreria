6cab40d588973c5db3da1291b9c10d3436b0f17b
Damián Mansilla <Damin Mansilla>
2016-06-22 02:22:57 -0300

    Nueva interfaz, se agrego a index.js donde dice agregado a borrar la posibilidad de buscar llamando a /prueba/nombreDelLibro todavia no esta implementado el modal y que sea posible hacerle clic, falta ajustar NAV

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index.jade');
+    console.log("Pagina principal");
 });
+
+
 
 router.get('/:title',function(req,res, next){
 
@@ -27,5 +31,17 @@ router.get('/:title',function(req,res, next){
 
 	});
 });
+
+//agregado borrar
+var Client = require('node-rest-client').Client;
+var client = new Client();
+router.get('/prueba/:titulo', function(req,res,next){
+    console.log("Prueba borrar");
+    client.get("https://www.googleapis.com/books/v1/volumes?q="+req.params.titulo, function(data,response){
+        res.render("listadoLibros.jade", {valores:data["items"]});
+    });
+});
+
+
 
 module.exports = router;
