74f8e7498b4a968161ad3c28a0625314efbcf8a8
Damián Mansilla <Damin Mansilla>
2016-06-25 01:11:25 -0300

    otros cambios

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -55,35 +55,52 @@ function mainController($scope, $http) {
                 Author = "Autor no disponible";
                 Description= "Descripcion no disponible";
                 Id="ERROR";//VER ESTO
+                Reactions={
+                    like: 0,
+                    angry: 0,
+                    wow: 0,
+                    haha: 0,
+                    sad:0
+                }
 
-               if (validar(item.saleInfo)){
-                    if (validar(item.saleInfo.listPrice)){
-                        if (validar(item.saleInfo.listPrice.amount)){
-                            Precio = item.saleInfo.listPrice.amount;
+                if (validar(item.id_google)){
+                    //Esta en local
+                    Precio = item.precios_locales;
+                    Title = item.title;
+                    Id = item.id_google;
+                    Reactions = item.reactions; //VERTODO esto, falta el thumbnail, languagem auhor, description
+
+                }else{//esta en la api de google
+
+                   if (validar(item.saleInfo)){
+                        if (validar(item.saleInfo.listPrice)){
+                            if (validar(item.saleInfo.listPrice.amount)){
+                                Precio = item.saleInfo.listPrice.amount;
+                            }
                         }
-                    }
-               }; //buscar la forma de hacer mas simple en un Y
-               
-               if (validar(item.volumeInfo)){
-                    if (validar(item.volumeInfo.imageLinks) && validar(item.volumeInfo.imageLinks.thumbnail)){
-                        Thumbnail = item.volumeInfo.imageLinks.thumbnail;
-                    }
-                    if (validar(item.volumeInfo.language)){
-                        Language = item.volumeInfo.language;
-                    }
-                    if (validar(item.volumeInfo.authors)){
-                        Author= item.volumeInfo.authors[0]; //ver
-                    }
-                    if (validar(item.volumeInfo.description)){
-                        Description = item.volumeInfo.description;
-                    }
-                    if (validar(item.volumeInfo.title)){
-                        Title = item.volumeInfo.title;
-                    }
-               } 
-               if (validar(item.id)){
-                Id = item.id;
-               }
+                   }; //buscar la forma de hacer mas simple en un Y
+                   
+                   if (validar(item.volumeInfo)){
+                        if (validar(item.volumeInfo.imageLinks) && validar(item.volumeInfo.imageLinks.thumbnail)){
+                            Thumbnail = item.volumeInfo.imageLinks.thumbnail;
+                        }
+                        if (validar(item.volumeInfo.language)){
+                            Language = item.volumeInfo.language;
+                        }
+                        if (validar(item.volumeInfo.authors)){
+                            Author= item.volumeInfo.authors[0]; //ver
+                        }
+                        if (validar(item.volumeInfo.description)){
+                            Description = item.volumeInfo.description.substring(0,250);
+                        }
+                        if (validar(item.volumeInfo.title)){
+                            Title = item.volumeInfo.title;
+                        }
+                   } 
+                   if (validar(item.id)){
+                    Id = item.id;
+                   }
+                }
                 $scope.LibrosResultados.push({
                     thumbnail: Thumbnail,
                     title: Title,
